dfb5841d17b0a4dae3c3c405c97220341887c9fd
Christopher Frost <git@codewax.org>
2015-08-28 15:35:26 +0100

    Adding support for preferences
    Some initial preference to configure the behaviour of the extension

--- data/tasks.js
+++ data/tasks.js
@@ -4,8 +4,9 @@
 	module.exports = function (rtm, button) {
 
 		var storage = require("sdk/simple-storage").storage,
-			self = require("sdk/self"),
 			tabs = require("sdk/tabs"),
+			self = require("sdk/self"),
+			preferences = require("sdk/simple-prefs").prefs,
 			Panel = require("sdk/panel").Panel;
 
 		var addTaskPanel = new Panel({
@@ -14,15 +15,17 @@
 			contentStyleFile: [self.data.url("views/common.css"),
 							   self.data.url("views/add/addTask.css")],
 			position: button,
-			height: 150,
-			width: 300,
+			height: 300,
+			width: 350,
 			onHide: function () {
 				button.state("window", {
 					checked: false
 				});
 			},
 			onShow: function () {
-				addTaskPanel.port.emit("update-page-details", tabs.activeTab.title, tabs.activeTab.url);
+				var title = preferences.useTitle ? tabs.activeTab.title : "";
+				var link = preferences.useLink ? tabs.activeTab.url : "";
+				addTaskPanel.port.emit("update-page-details", title, link);
 			}
 		});
 
@@ -32,7 +35,67 @@
 
 		addTaskPanel.port.on("save-task", function (task, url) {
 			console.log("Port.on(save-task): " + task + " url: " + url);
+			var list = preferences.defaultList;
+			if (list === null || list === "") {
+				list = "Inbox";
+			}
+			rtm.tasks.add
 		});
+
+		this.addTask = function (name, list_id) {
+			rtm.get('rtm.tasks.add', {
+					list_id: list_id,
+					name: name
+				},
+				function (resp) {
+					button.state("window", {
+						checked: false
+					});
+					storage.frob = resp.rsp.frob;
+					var authUrl = rtm.getAuthUrl(storage.frob);
+					console.log("authUrl: " + authUrl);
+
+					rtm.get('rtm.tasks.setURL', {
+							frob: storage.frob
+						},
+						function (resp) {
+							rtm.setAuthToken(resp.rsp.auth.token);
+							console.log("token: " + resp);
+							storage.token = resp.rsp.auth.token;
+						}
+					);
+				},
+				function (response) {
+					console.log("Network Error: " + response.status + "-" + response.statusText);
+				}
+			);
+		};
+
+		this.getListId = function (listName) {
+			rtm.get('rtm.lists.getList', {},
+				function (resp) {
+					var lists = resp.rsp.lists;
+
+				},
+				function (response) {
+					console.log("Network Error: " + response.status + "-" + response.statusText);
+				}
+			);
+		};
+
+		this.addList = function (name) {
+			rtm.get('rtm.lists.add', {
+					timeline: "",
+					name: ""
+				},
+				function (resp) {
+					var newList = resp.rsp.list;
+				},
+				function (response) {
+					console.log("Network Error: " + response.status + "-" + response.statusText);
+				}
+			);
+		};
 
 	};
 
