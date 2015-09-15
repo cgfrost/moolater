2c01e8963bf0539f1f000242422c7d261657ee91
Christopher Frost <git@codewax.org>
2015-09-15 18:33:53 +0100

    New MD5 algorithim
    Moving to a new MD5 algorithim that can support 2 byte char sets. Before, any task description with chars like £ or – in the description would fail due to a bad api signature.

---
+++ test/test-md5.js
@@ -0,0 +1,35 @@
+(function () {
+	'use strict';
+
+	let test = require('sdk/test');
+	let {
+		before, after
+	} = require('sdk/test/utils');
+	let md5 = require('../data/md5');
+
+	exports['test hashing a simple string'] = function (assert) {
+		assert.strictEqual(`FOO${md5('simple string')}`, 'FOObec0124123e5ab4c2ce362461cb46ff0', 'Bad hash generated.');
+	};
+
+	exports['test hashing a harder string'] = function (assert) {
+		assert.strictEqual(`FOO${md5('harder string :%"')}`, 'FOO7582706eff976e8f64b5d8a0b6a388d8', 'Bad hash generated.');
+	};
+
+	exports['test hashing a multi byte char'] = function (assert) {
+		console.log(`FOO${md5('multi byte chars |–£')}`);
+		assert.strictEqual(`FOO${md5('multi byte chars |–£')}`, 'FOO75f71999196ad059252f484bf52577fc', 'Bad hash generated.');
+	};
+
+	before(exports, function (name) {
+		console.log('================================================================================');
+		console.log(`Running md5: ${name}`);
+	});
+
+	after(exports, function (name) {
+		console.log(`Finished md5: ${name}`);
+		console.log('================================================================================');
+	});
+
+	test.run(exports);
+
+}());
