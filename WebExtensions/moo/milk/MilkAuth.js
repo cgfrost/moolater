42a503409aeddbb0f1c0fb3becde841f91abcbe8
Christopher Frost <git@codewax.org>
2016-11-06 21:42:49 +0000

    Moving code over
    Starting work on options and settings. Need to finish this then move on to getting the back ground script to compile and load popups.

---
+++ WebExtensions/moo/milk/MilkAuth.js
@@ -0,0 +1,40 @@
+(function () {
+	'use strict';
+	
+	module.exports = {	
+		
+		getToken: (milk) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.auth.getToken', {},
+					resolve,
+					reject);
+			});
+		},
+		
+		checkToken: (milk) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.auth.checkToken', {},
+					resolve,
+					reject);
+			});
+		},
+
+		getFrob: (milk) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.auth.getFrob', {},
+					resolve,
+					reject);
+			});
+		},
+
+		createTimeline: (milk) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.timelines.create', {},
+					resolve,
+					reject);
+			});
+		}
+		
+	};
+	
+}());
---
+++ WebExtensions/moo/milk/MilkTasks.js
@@ -0,0 +1,71 @@
+(function () {
+	'use strict';
+
+	let preferences = require('sdk/simple-prefs').prefs;
+
+	module.exports = {
+
+		addTask: (milk, name, listId) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.tasks.add', {
+						list_id: listId,
+						name: name,
+						timeline: milk.timeline,
+						parse: preferences['extensions.moolater.useSmartAdd'] ? 1 : 0
+					},
+					resolve,
+					reject);
+			});
+		},
+
+		addUrlToTask: (milk, task, link) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.tasks.setURL', {
+						list_id: task.id,
+						taskseries_id: task.taskseries.id,
+						task_id: task.taskseries.task.id,
+						url: link,
+						timeline: milk.timeline
+					},
+					resolve,
+					reject);
+			});
+		},
+
+		addNoteToTask: (milk, task, title, text) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.tasks.notes.add', {
+						list_id: task.id,
+						taskseries_id: task.taskseries.id,
+						task_id: task.taskseries.task.id,
+						timeline: milk.timeline,
+						note_title: title,
+						note_text: text
+					},
+					resolve,
+					reject);
+			});
+		},
+
+		getLists: (milk) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.lists.getList', {},
+					resolve,
+					reject);
+			});
+		},
+
+		addList: (milk, name) => {
+			return new Promise((resolve, reject) => {
+				milk.get('rtm.lists.add', {
+						name: name,
+						timeline: milk.timeline
+					},
+					resolve,
+					reject);
+			});
+		}
+
+	};
+
+}());
