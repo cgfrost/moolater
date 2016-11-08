59774abf8bc143a958c9c37246f4de66dc6abad9
Christopher Frost <git@codewax.org>
2016-11-08 23:48:46 +0000

    Styling and fixing settings page

--- WebExtensions/settings/options.js
+++ WebExtensions/settings/options.js
@@ -1,13 +1,37 @@
 function saveOptions(e) {
-  chrome.storage.local.set({
-    color: document.querySelector("#color").value
-  });
+    chrome.storage.local.set({
+        defaultList: document.querySelector("#moolater-defaultList").value
+    });
+    chrome.storage.local.set({
+        useTitle: document.querySelector("#moolater-useTitle").value
+    });
+    chrome.storage.local.set({
+        useLink: document.querySelector("#moolater-useLink").value
+    });
+    chrome.storage.local.set({
+        useSmartAdd: document.querySelector("#moolater-useSmartAdd").value
+    });
+    chrome.storage.local.set({
+        showContextMenu: document.querySelector("#moolater-showContextMenu").value
+    });
 }
 
 function restoreOptions() {
-  chrome.storage.local.get("color", (res) => {
-    document.querySelector("#color").value = res.color || "blue";
-  });
+    chrome.storage.local.get("moolater-defaultList", (res) => {
+        document.querySelector("#moolater-defaultList").value = res.defaultList || "Read Later";
+    });
+    chrome.storage.local.get("moolater-useTitle", (res) => {
+        document.querySelector("#moolater-useTitle").value = res.useTitle || true;
+    });
+    chrome.storage.local.get("moolater-useLink", (res) => {
+        document.querySelector("#moolater-useLink").value = res.useLink || true;
+    });
+    chrome.storage.local.get("moolater-useSmartAdd", (res) => {
+        document.querySelector("#moolater-useSmartAdd").value = res.useSmartAdd || true;
+    });
+    chrome.storage.local.get("moolater-showContextMenu", (res) => {
+        document.querySelector("#moolater-showContextMenu").value = res.showContextMenu || true;
+    });
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
