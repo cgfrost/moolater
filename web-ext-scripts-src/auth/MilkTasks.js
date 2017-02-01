00f04388c2d897febc890dff03d93f0a41fdbe7e
Christopher Frost <git@codewax.org>
2017-02-01 15:36:55 +0000

    MilkTasks updates

--- web-ext-scripts-src/auth/MilkTasks.js
+++ web-ext-scripts-src/auth/MilkTasks.js
@@ -8,7 +8,7 @@ export default class {
 		return new Promise((resolve, reject) => {
 			browser.storage.local.get("moolater-useSmartAdd")
 				.then(result => {
-					milk.get('rtm.tasks.add', {
+					milk.queryRTM('rtm.tasks.add', {
 						list_id: listId,
 						name: name,
 						timeline: milk.timeline,
@@ -22,7 +22,7 @@ export default class {
 
 	addUrlToTask(milk, task, link) {
 		return new Promise((resolve, reject) => {
-			milk.get('rtm.tasks.setURL', {
+			milk.queryRTM('rtm.tasks.setURL', {
 					list_id: task.id,
 					taskseries_id: task.taskseries.id,
 					task_id: task.taskseries.task.id,
@@ -36,7 +36,7 @@ export default class {
 
 	addNoteToTask(milk, task, title, text) {
 		return new Promise((resolve, reject) => {
-			milk.get('rtm.tasks.notes.add', {
+			milk.queryRTM('rtm.tasks.notes.add', {
 					list_id: task.id,
 					taskseries_id: task.taskseries.id,
 					task_id: task.taskseries.task.id,
@@ -51,7 +51,7 @@ export default class {
 
 	getLists(milk) {
 		return new Promise((resolve, reject) => {
-			milk.get('rtm.lists.getList', {},
+			milk.queryRTM('rtm.lists.getList', {},
 				resolve,
 				reject);
 		});
@@ -59,7 +59,7 @@ export default class {
 
 	addList(milk, name) {
 		return new Promise((resolve, reject) => {
-			milk.get('rtm.lists.add', {
+			milk.queryRTM('rtm.lists.add', {
 					name: name,
 					timeline: milk.timeline
 				},
