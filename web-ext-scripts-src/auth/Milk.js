ac3f7204235c2b8498a9ef4d6e984f255c571626
Christopher Frost <git@codewax.org>
2017-02-01 15:36:39 +0000

    Milk updates

--- web-ext-scripts-src/auth/Milk.js
+++ web-ext-scripts-src/auth/Milk.js
@@ -9,16 +9,50 @@ const FORMAT = 'json';
 export default class {
 
 	constructor(data, permissions) {
+		console.log("Milk");
 		this.milkAuth = new MilkAuth();
 		this.permissions = (permissions) ? permissions : 'write';
 		if (!data.a || !data.b) {
 			throw 'Milk Error: Missing data.';
 		}
 		this.data = data;
+
+		browser.storage.local.get(["token", "frob"]).then(
+			(result) => {
+				console.log(`Milk init: In storage frob is ${result.frob} and the token is ${result.token} `)
+				if(result.frob) {
+					this.frob = result.frob;
+					if (result.token) {
+						this.token = result.token;
+						this.milkAuth.checkToken(this).then(
+							() => {},
+							() => {
+								this.wipeToken();
+								this.wipeFrob();
+								this.setFrob();
+							});
+					} else {
+						this.fetchToken(
+							() => {}),
+							(error) => {
+								console.log(`Milk constructor - fetchToken: ${error}`);
+							};
+					}
+				} else {
+					this.setFrob();
+				}
+			}, (error) => {
+				console.log(`Milk constructor: ${error}`);
+			});
 	}
 
-	getUserAuthenticated(callback, onError) {
-		browser.storage.local.get(["token", "frob"]).then(callback, onError);
+	isUserReady() {
+		if(this.frob && this.token) {
+			this.setTimeline();
+			return true;
+		} else{
+			return false;
+		}
 	}
 
 	/**
@@ -35,131 +69,151 @@ export default class {
 		return AUTH_URL + this.encodeUrlParams(params);
 	}
 
-	// events.on('token.init', () => {
-	// 	this.setTimeline();
-	// });
-
 	/**
 	 * Gets the timeline ID
 	 *
 	 * @return     Returns the timline ID String
 	 */
-	setTimeline() {
-		milkAuth.createTimeline(this).then((response) => {
-			this.timeline = response.rsp.timeline;
-		}).catch((reason) => {
-			console.warn(reason);
-		});
+	setTimeline(error) {
+		this.milkAuth.createTimeline(this).then(
+			(response) => {
+				this.timeline = response.rsp.timeline;
+			},
+			(error) => {
+				console.log(`Milk setTimeline: ${error}`);
+			});
 	}
 
-		// setFrob(frob) {
-		// 	storage.frob = frob;
-		// 	this.frob = frob;
-		// }
-		//
-		// hasFrob() {
-		// 	return (this.frob && storage.frob);
-		// }
+	/**
+	 * Obtain a new frob from RTM and save it in storage
+	 *
+	 * @return the new frob
+	 */
+	setFrob() {
+		this.milkAuth.getFrob(this).then(
+			(response) => {
+				this.frob = response.rsp.frob
+				browser.storage.local.set({
+		        frob: response.rsp.frob
+		    });
+			},
+			(error) => {
+				console.log(`Milk setFrob: ${error}`);
+			});
+	}
 
-		/**
-		 * Main method for making API calls
-		 *
-		 * @param method    Specifies what API method to be used
-		 * @param params    Array of API parameters to accompany the method parameter
-		 * @param complete  Callback to fire after the request comes back
-		 * @param error     (Optional) Callback to fire if the request fails
-		 * @return          Returns the reponse from the RTM API
-		 */
-		get(method, params, complete, error) {
-			if (!method) {
-				throw 'Error: API Method must be defined.';
-			}
+	wipeFrob() {
+		console.log("Wipe Frob");
+		browser.storage.local.remove('frob');
+		this.frob = null;
+	}
 
-			if (!params) {
-				throw 'Error: API Params must be defined.';
-			}
+	wipeToken() {
+		console.log("Wipe Token");
+		browser.storage.local.remove('token');
+		this.token = null;
+	}
 
-			if (!complete) {
-				throw 'Error: API Complete function must be defined.';
-			}
+	/**
+	 * Main method for making API calls
+	 *
+	 * @param method    Specifies what API method to be used
+	 * @param params    Array of API parameters to accompany the method parameter
+	 * @param complete  Callback to fire after the request comes back
+	 * @param error     (Optional) Callback to fire if the request fails
+	 * @return          Returns the reponse from the RTM API
+	 */
+	queryRTM(method, params, complete, error) {
+		console.log(`Calling RTM with ${method}`);
 
-			if (!error) {
-				error = function () {};
-			}
+		if (!method) {
+			throw 'Error: API Method must be defined.';
+		}
+		if (!params) {
+			throw 'Error: API Params must be defined.';
+		}
+		if (!complete) {
+			throw 'Error: API Complete function must be defined.';
+		}
+		if (!error) {
+			error = function () {};
+		}
 
-			params.v = API_VERSION;
-			params.format = FORMAT;
-			params.method = method;
+		params.v = API_VERSION;
+		params.format = FORMAT;
+		params.method = method;
 
-			if (this.auth_token) {
-				params.auth_token = this.auth_token;
-			}
+		if (this.token) {
+			params.auth_token = this.token;
+		}
 
-			if (this.frob) {
-				params.frob = this.frob;
-			}
+		if (this.frob) {
+			params.frob = this.frob;
+		}
 
-			var requestUrl = BASE_URL + this.encodeUrlParams(params);
+		let requestUrl = BASE_URL + this.encodeUrlParams(params);
+		let xhttp = new XMLHttpRequest();
 
-			new Request({
-				url: requestUrl,
-				overrideMimeType: 'application/json; charset=utf-8',
-				onComplete: (response) => {
-										console.log('*************************************');
-//										console.log(`Request.Url     : ${requestUrl}`);
-										console.log(`Request.Method  : ${method}`);
-										console.log(`Response.Text   : ${response.text}`);
-										console.log(`        .Status : ${response.status}`);
-										console.log(`        .Text   : ${response.statusText}`);
-										console.log('*************************************');
-					if (response.status === 200 && response.json.rsp.stat === 'ok') {
-						complete(response.json);
+		xhttp.open('POST', requestUrl, true);
+		xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+		xhttp.onreadystatechange = () => {
+			if (xhttp.readyState == 4){
+				console.log('*************************************');
+				console.log(`Request.Url           : ${requestUrl}`);
+				console.log(`Request.Method        : ${method}`);
+				console.log(`Response.status       : ${xhttp.status}`);
+				console.log(`        .statusText   : ${xhttp.statusText}`);
+				console.log(`        .responseText : ${xhttp.responseText}`);
+				console.log('*************************************');
+				if (xhttp.status == 200) {
+					let jsonResponse = JSON.parse(xhttp.responseText);
+					if (jsonResponse.rsp.stat === 'ok') {
+						complete(jsonResponse);
 					} else {
-						this.handleError(response, error, () => {
-							this.get(method, params, complete, error);
+						this.handleError(jsonResponse, error, () => {
+							this.queryRTM(method, params, complete, error);
 						});
 					}
-				}
-			}).get();
-		}
+				} else {
+					error(`Network Error:${response.status} ${response.statusText}`);
+				};
+			};
+		};
+		xhttp.send();
+	}
 
-		handleError(response, error, retry) {
-			if (response.status === 200) {
-				var rsp = response.json.rsp;
-				if (rsp.err && rsp.err.msg && rsp.err.code) {
-					if (rsp.err.code === '98') {
-						// storage.token = null;
-						this.auth_token = null;
-						this.fetchToken(retry, error);
-						return;
-					} else if (rsp.err.code === '101') {
-						// storage.token = null;
-						this.auth_token = null;
-						// storage.frob = null;
-						this.frob = null;
-					}
-				}
-				error(`Error ${rsp.err.code}: ${rsp.err.msg}`);
-			} else {
-				error(`Network Error:${response.status} ${response.statusText}`);
+	handleError(response, error, retry) {
+		var rsp = response.rsp;
+		if (rsp.err && rsp.err.msg && rsp.err.code) {
+			if (rsp.err.code === '98') {
+				this.wipeToken();
+				this.fetchToken(retry, error);
+				return;
+			} else if (rsp.err.code === '101') {
+				this.wipeToken();
+				this.wipeFrob();
 			}
 		}
+		error(`Error ${rsp.err.code}: ${rsp.err.msg}`);
+	}
 
-		fetchToken(retry, error) {
-			milkAuth.getToken(this)
-				.then((resp) => {
-					this.auth_token = resp.rsp.auth.token;
-					// storage.token = resp.rsp.auth.token;
-					// events.do('token.init');
-					if (retry) {
-						retry();
-					}
-				}).catch((reason) => {
-					if (error) {
-						error(reason);
-					}
-				});
-		}
+	fetchToken(retry, error) {
+		this.milkAuth.getToken(this)
+			.then((response) => {
+				this.token = response.rsp.auth.token;
+				browser.storage.local.set({
+		        token: response.rsp.auth.token
+		    });
+				this.setTimeline(error);
+				if (retry) {
+					retry();
+				}
+			}).catch((reason) => {
+				if (error) {
+					error(reason);
+				}
+			});
+	}
 
 		/**
 		 * Private - Encodes request parameters into URL format
@@ -195,15 +249,12 @@ export default class {
 		 */
 		generateSig(params) {
 			params = (params) ? params : {};
-			var signature = '',
-				keys = Object.keys(params);
-
+			var signature = '', keys = Object.keys(params);
 			keys.sort();
 			for (var i = 0; i < keys.length; i++) {
 				signature += keys[i] + params[keys[i]];
 			}
 			signature = this.data.b + signature;
-
 			return `&api_sig=${md5(signature)}`;
 		}
 
